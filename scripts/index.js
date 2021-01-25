2f6dc5bc2694b7ab4e1d3ad3794d4d4222cfb34a
utkarsh-UK <utkarshkore98@gmail.com>
2021-01-25 21:02:59 +0530

    Completing challenges

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 const shareBtn = document.getElementById("share-profile");
 let isTooltipShowing = false;
 
+const closeCurrentTooltip = () => {
+  const tooltip = document.querySelector(".tooltip-class");
+  tooltip.remove();
+  isTooltipShowing = false;
+};
+
 const shareProfileHandler = () => {
   if (isTooltipShowing) return;
 
@@ -9,15 +15,17 @@ const shareProfileHandler = () => {
   const containerTitle = document.querySelector(".description-container h4");
   const containerDesc = document.querySelector(".description-container p");
   const titleBottomRight = containerTitle.getBoundingClientRect().right;
-  console.log(titleBottomRight);
 
   const tooltip = document.importNode(tooltipTemplate.content, true);
   tooltipElement.append(tooltip);
 
   tooltipElement.style.position = "absolute";
+  tooltipElement.style.left = `${titleBottomRight - 100}px`;
+  tooltipElement.className = "tooltip-class";
 
   containerDesc.insertAdjacentElement("beforebegin", tooltipElement);
   isTooltipShowing = true;
 };
 
 shareBtn.addEventListener("mouseover", shareProfileHandler);
+shareBtn.addEventListener("mouseleave", closeCurrentTooltip);
